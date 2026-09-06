7bec09710f8608409c3d1a287265d6cd3140e0a7
Akhilesh <akhipn4753@gmail.com>
2026-09-06 16:24:49 +0530

    added google analatics

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
-import React, { useState } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, useLocation } from 'react-router-dom';
 import { ThemeProvider } from './context/ThemeContext';
 import { AppRoutes } from './routes/AppRoutes';
 import { LoadingScreen } from './components/common/LoadingScreen';
+
+function AnalyticsTracker() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (typeof window !== 'undefined' && typeof window.gtag === 'function') {
+      window.gtag('config', 'G-8CRCHQSJM4', {
+        page_path: location.pathname + location.search + location.hash
+      });
+    }
+  }, [location]);
+
+  return null;
+}
 
 export function App() {
   const [loading, setLoading] = useState(true);
@@ -11,6 +25,7 @@ export function App() {
     <ThemeProvider>
       {loading && <LoadingScreen onComplete={() => setLoading(false)} />}
       <BrowserRouter>
+        <AnalyticsTracker />
         <AppRoutes />
       </BrowserRouter>
     </ThemeProvider>
